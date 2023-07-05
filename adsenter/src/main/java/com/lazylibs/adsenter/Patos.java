5860cb3afd77bb53513498ff3da95ba312cb168f
lazy2b <reizelibs@gmail.com>
2023-07-05 16:15:45 +0800

    压缩代码

--- adsenter/src/main/java/com/lazylibs/adsenter/Patos.java
+++ adsenter/src/main/java/com/lazylibs/adsenter/Patos.java
@@ -1,15 +1,11 @@
 package com.lazylibs.adsenter;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Html;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AnimationUtils;
-import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.ViewFlipper;
 
@@ -29,8 +25,8 @@ public class Patos extends AppCompatActivity {
         viewFlipper = new ViewFlipper(this);
         viewFlipper.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
         setContentView(viewFlipper);
-        viewFlipper.setInAnimation(AnimationUtils.loadAnimation(viewFlipper.getContext(), R.anim.push_left_in));
-        viewFlipper.setOutAnimation(AnimationUtils.loadAnimation(viewFlipper.getContext(), R.anim.push_right_out));
+//        viewFlipper.setInAnimation(AnimationUtils.loadAnimation(viewFlipper.getContext(), R.anim.push_left_in));
+//        viewFlipper.setOutAnimation(AnimationUtils.loadAnimation(viewFlipper.getContext(), R.anim.push_right_out));
         vLoading = LayoutInflater.from(this).inflate(R.layout.tpl_loading, viewFlipper, false);
         if (Enter.isAgreePatos(this)) {
             v2Main();
@@ -39,26 +35,16 @@ public class Patos extends AppCompatActivity {
         }
     }
 
-    private View vLoading;
-    private FrameLayout vPatos;
-    private FrameLayout.LayoutParams layoutParams;
+    private View vPatos, vLoading;
 
     private void v2Patos() {
 
         viewFlipper.addView(vLoading);
         viewFlipper.showNext();
 
-        vPatos = new FrameLayout(this);
-        vPatos.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT));
-
-        Button tvAgree = new Button(this);
-        layoutParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT);
-        layoutParams.gravity = Gravity.CENTER | Gravity.BOTTOM;
-        layoutParams.bottomMargin = Enter.dp(this, 20);
-        tvAgree.setLayoutParams(layoutParams);
-        tvAgree.setGravity(Gravity.CENTER);
-        tvAgree.setBackgroundColor(Color.RED);
-        tvAgree.setText(R.string.agree);
+        vPatos = LayoutInflater.from(this).inflate(R.layout.tpl_patos, viewFlipper, false);
+        TextView tvAgree = vPatos.findViewById(R.id.tv_agree);
+        TextView tvPatos = vPatos.findViewById(R.id.tv_patos);
         tvAgree.setOnClickListener(v -> {
             Enter.setAgreePatos(this);
             v2Main();
@@ -66,25 +52,17 @@ public class Patos extends AppCompatActivity {
 
         String patos = Enter.skipper().getPatos();
         if (patos.startsWith("http://") || patos.startsWith("https://")) {
-            View wbPatos = new FrameLayout(this);
-            wbPatos.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT));
-            wbPatos.setId(View.generateViewId());
-            vPatos.addView(wbPatos);
-            FragmentUtils.replaceFragmentToActivity(wbPatos.getId(), getSupportFragmentManager(), LazyWebFragment.newInstance(Enter.skipper().getPatos(), null).setExtraWebHandler(new IWebHandler() {
+            vPatos.findViewById(R.id.wb_patos).setVisibility(View.VISIBLE);
+            FragmentUtils.replaceFragmentToActivity(R.id.wb_patos, getSupportFragmentManager(), LazyWebFragment.newInstance(patos, null).setExtraWebHandler(new IWebHandler() {
                 @Override
                 public void onRealPageFinished(String url, boolean isReceivedError) {
-                    vPatos.addView(tvAgree);
+                    tvAgree.setVisibility(View.VISIBLE);
                 }
             }));
         } else {
-            TextView tvPatos = new TextView(this);
-            layoutParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
-            layoutParams.gravity = Gravity.CENTER;
-            tvPatos.setLayoutParams(layoutParams);
-            tvPatos.setGravity(Gravity.CENTER);
             tvPatos.setText(Html.fromHtml(patos, Html.FROM_HTML_MODE_COMPACT));
-            vPatos.addView(tvPatos);
-            vPatos.addView(tvAgree);
+            tvPatos.setVisibility(View.VISIBLE);
+            tvAgree.setVisibility(View.VISIBLE);
         }
         viewFlipper.addView(vPatos);
         viewFlipper.showNext();
